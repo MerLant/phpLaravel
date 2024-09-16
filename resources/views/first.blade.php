5558ca47c999a3fd4dbd6b9a4be2764d7bcb866c
MerLant <merlantpc@gmail.com>
2024-09-16 11:56:36 +0500

    first commit

---
+++ resources/views/first.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Первая страница</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <p>Первая страница</p>
+    {{ $c }}
+
+</body>
+
+</html>
